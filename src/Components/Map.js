29bd16c4e93aaf53e24ad98394d2b010a7301c8f
mhchan0 <92717936+cmh2ano@users.noreply.github.com>
2022-12-16 06:29:36 +0800

    Making map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -15,26 +15,51 @@ class Map extends Component {
         this.state = {
             lng: this.props.lng,
             lat: this.props.lat,
-            zoom: this.props.zoom
+            zoom: this.props.zoom,
         };
         this.mapContainer = React.createRef();
     }
 
     componentDidMount() {
-        const { lng, lat, zoom } = this.state;
-        const map = new mapboxgl.Map({
-            container: this.mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [lng, lat],
-            zoom: zoom
-        });
+        this.LoadLocationList();
+    }
 
-        map.on('move', () => {
-            this.setState({
-                lng: map.getCenter().lng.toFixed(4),
-                lat: map.getCenter().lat.toFixed(4),
-                zoom: map.getZoom().toFixed(2)
+    LoadLocationList() {
+
+        axios({
+            // need change localhost to the publicIP
+            url: "http://localhost:8080/location",
+            method: "POST",
+        })
+        .then((r) => {    
+            const { lng, lat, zoom } = this.state;
+            const map = new mapboxgl.Map({
+                container: this.mapContainer.current,
+                style: 'mapbox://styles/mapbox/streets-v12',
+                center: [lng, lat],
+                zoom: zoom
             });
+
+            map.on('move', () => {
+                this.setState({
+                    lng: map.getCenter().lng.toFixed(4),
+                    lat: map.getCenter().lat.toFixed(4),
+                    zoom: map.getZoom().toFixed(2)
+                });
+            });
+
+            map.addControl(new mapboxgl.FullscreenControl({container: document.querySelector('mapbox-map')}));
+            
+            for (let i of r.data) {
+                new mapboxgl.Marker()
+                .setLngLat([i.longitude, i.latitude])
+                .setPopup(new mapboxgl.Popup().setHTML(`<h5>${i.name}</h5><h6>Event Number: ${i.programme.length}</h6>`))
+                .addTo(map);
+            }
+
+        })
+        .catch((err) => {
+            console.log("Internal server error");
         });
     }
 
@@ -45,7 +70,8 @@ class Map extends Component {
                 <div className="map-sidebar">
                     Longitude:&nbsp;{lng} | Latitude:&nbsp;{lat} | Zoom:&nbsp;{zoom}
                 </div>
-                <div ref={this.mapContainer} className="map-container" />
+                <div id="mapbox-map" ref={this.mapContainer} className="map-container">
+                </div>
             </div>
         );
     }
